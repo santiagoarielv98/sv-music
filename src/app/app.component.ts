a0e044720f20bd6967b930e124a9e7cad2f9ed5b
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-12 20:16:31 -0300

    remove mat-slide-toggle and mat-button imports; simplify app.component structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSlideToggleModule, MatButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
