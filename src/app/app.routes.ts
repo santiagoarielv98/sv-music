a1eb14e6aa1657b9e21de752992e6e9f0024ba20
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-13 10:19:49 -0300

    add home and library pages; update routing and remove search page components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/pages/home-page/home-page.component').then(
+        (m) => m.HomePageComponent,
+      ),
+  },
+  {
+    path: 'search',
+    loadChildren: () =>
+      import('./features/search/search.routes').then((m) => m.SearchModule),
+  },
+  {
+    path: 'library',
+    loadComponent: () =>
+      import(
+        './features/library/pages/library-page/library-page.component'
+      ).then((m) => m.LibraryPageComponent),
+  },
+  { path: '**', redirectTo: '' },
+];
