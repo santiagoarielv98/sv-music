fb9152a446d9960144377d150027fe72bd2cb208
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-13 10:16:05 -0300

    add core services and feature components for home, library, and search pages

---
+++ src/app/features/library/page/library-page/library-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-library-page',
+  imports: [],
+  templateUrl: './library-page.component.html',
+  styleUrl: './library-page.component.css'
+})
+export class LibraryPageComponent {
+
+}
